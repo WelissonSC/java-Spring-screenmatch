b70f0300fd4ec1f0711b7d8a5cff6f76b5b04abb
WelissonSC <welissonsilvacunha@gmail.com>
2025-06-09 12:36:38 -0300

    News updates of curse spring on this project

--- src/main/java/com/ws/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/ws/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package com.ws.screenmatch;
 
+import com.ws.screenmatch.main.Main;
+import com.ws.screenmatch.model.EpisodesData;
+import com.ws.screenmatch.model.SeasonData;
 import com.ws.screenmatch.model.SerieData;
 import com.ws.screenmatch.service.ApiConsumer;
 import com.ws.screenmatch.service.DataConvert;
@@ -7,20 +10,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		ApiConsumer apiConsumer = new ApiConsumer();
-		String json = apiConsumer.catData("https://www.omdbapi.com/?t=Whiplash&apikey=be919000");
-		//System.out.println(json);
-		DataConvert convert = new DataConvert();
-		SerieData data = convert.catData(json, SerieData.class);
-		System.out.println(data);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+
+        Main main = new Main();
+        main.showMenu();
+
+    }
 }
---
+++ src/main/java/com/ws/screenmatch/model/EpisodesData.java
@@ -0,0 +1,11 @@
+package com.ws.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record EpisodesData(@JsonAlias("Title") String title,
+                           @JsonAlias("Episode") Integer number,
+                           @JsonAlias("imdbRating") String rate,
+                           @JsonAlias("Released") String dateRelease) {
+}
